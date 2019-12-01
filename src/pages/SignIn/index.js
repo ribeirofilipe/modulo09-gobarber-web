850830fc2faf08e6bb6e7ca306be917296bd57cf
Filipe de Oliveira Ribeiro <foribeiro@viceri.com.br>
2019-12-01 19:13:17 -0300

    chore - inserido redux e autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import { signInRequest } from '~/store/modules/auth/actions';
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('e-mail inválido.')
+    .required('e-mail obrigatório.'),
+  password: Yup.string().required('senha obrigatória'),
+});
+
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Form, Input } from '@rocketseat/unform';
-
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('nome obrigatório.'),
+  email: Yup.string()
+    .email('e-mail inválido.')
+    .required('e-mail obrigatório.'),
+  password: Yup.string()
+    .min(6, 'minimo 6 digitos.')
+    .required('senha obrigatória'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -13,7 +23,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
